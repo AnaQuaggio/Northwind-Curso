f2dfb4c70fd9231eee29c1a03a867d610a0af344
Ana Lia Campos Quaggio <analiacq@gmail.com>
2024-12-27 14:07:33 +0000

    finalizada fato vendas com teste na metica total bruto

--- models/staging/erp/stg_erp__categorias.sql
+++ models/staging/erp/stg_erp__categorias.sql
@@ -1,7 +1,7 @@
 with
     renomeacao as (
         select
-            cast(ID as int) as pk_funcionario
+            cast(ID as int) as pk_categoria
             , cast(CATEGORYNAME as varchar) as nome_categoria
             , cast(DESCRIPTION as varchar) as descricao_categoria
            from {{ source('erp_northwind', 'category') }}                  
