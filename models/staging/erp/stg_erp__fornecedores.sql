f2aca4db511dc83edee3b8c6d2d9e7224f226c64
Ana Lia Campos Quaggio <analiacq@gmail.com>
2024-12-27 11:00:41 +0000

    finalizada dim_produtos

--- models/staging/erp/stg_erp__fornecedores.sql
+++ models/staging/erp/stg_erp__fornecedores.sql
@@ -1,12 +1,12 @@
 with
-    renomeacao as (
+    fonte_fornecedores as (
         select
             cast(ID as int) as pk_fornecedor
             , cast(COMPANYNAME as varchar) as nome_fornecedor
-            , cast(CITY as varchar) as cidade_forncedor
-            , cast(COUNTRY as varchar) as pais_forncedor
-           from {{ source('erp_northwind', 'supplier') }}                  
-            )
+            , cast(CITY as varchar) as cidade_fornecedor
+            , cast(COUNTRY as varchar) as pais_fornecedor
+        from {{ source('erp_northwind', 'supplier') }}
+    )
 
 select *
-from renomeacao
+from fonte_fornecedores
